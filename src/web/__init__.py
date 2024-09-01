a031f3e5b498e87529b60dfb839712c8db79cc14
Patricio Benjamin Serres <patricioserres@gmail.com>
2024-09-01 15:16:15 -0300

    Agrego error template

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from src.web.handlers import error 
 
 def create_app(env="development",static_folder=""):
     app = Flask(__name__)   
@@ -6,5 +7,7 @@ def create_app(env="development",static_folder=""):
     @app.route("/")
     def home():
         return "Hola Mundo!"
+    
+    app.register_error_handler(404, error.error_not_found)
     
     return app
---
+++ src/web/handlers/error.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from flask import render_template
+
+@dataclass
+class Error:
+    code: int
+    message: str
+    description: str
+
+def error_not_found(e):
+    error = Error(404, "Not Found", "The Requested URL was not found on the server.")
+    
+    return render_template('error.html', error=error), 404
