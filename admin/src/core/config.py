87945d50c4dd09dbb60bdf7c1df15a19d2f1cae3
repparw <ubritos@gmail.com>
2024-09-16 18:18:13 -0300

    Agr core skel

---
+++ admin/src/core/config.py
@@ -0,0 +1,25 @@
+class Config(object):
+    TESTING = False
+
+class ProductionConfig(Config):
+    pass
+
+class DevelopmentConfig(Config):
+    DB_USER = "postgres"
+    DB_PASS = "postgres"
+    DB_HOST = "localhost"
+    DB_PORT = "5432"
+    DB_NAME = "grupo10"
+    SQLALCHEMY_DATABASE_URI = (
+            f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+config = {
+        "development": DevelopmentConfig,
+        "testing": TestingConfig,
+        "production": ProductionConfig,
+        }
---
+++ admin/src/core/database.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def init_app(app):
+
+    db.init_app(app)
+    config(app)
+
+    return app
+
+def config(app):
+
+    @app_teardown_context
+    def close_session(exception=None)
+        db.session.close()
+
+    return app
+
+def reset():
+    # Drop and create with prints
+    print("Dropping tables")
+    db.drop_all()
+    print("Creating tables")
+    db.create_all()
+    print("Done")
