d8c054e17f07dd9e7a5300475ecac46c3dba992a
Ulises Britos <ubritos@gmail.com>
2024-09-20 17:23:16 -0300

    rm issues, rm seed, fix typos

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,15 +1,13 @@
 from flask import Flask
 from flask import render_template 
-from src.web.controllers.issues import bp as issues_bp
 from src.web.handlers import error 
 from src.core import database
-from src.core import seeds
 from src.core.config import config
 
 def create_app(env="development",static_folder="../../static"):
     app = Flask(__name__, static_folder=static_folder)   
 
-    app.config.From_obect(config[env])
+    app.config.from_object(config[env])
     database.init_app(app)
 
     @app.route("/")
@@ -20,16 +18,10 @@ def create_app(env="development",static_folder="../../static"):
     def about():
         return render_template("about.html")
 
-    app.register_blueprint(issues_bp)
-
     app.register_error_handler(404, error.error_not_found)
 
     @app.cli.command(name="reset-db")
     def reset_db():
         database.reset()
-
-    @app.cli.command(name="seeds-db")
-    def seeds_db():
-        seeds.run()
 
     return app
