77ecd5fa8635aa61b06a0c8ec92abd440e4054ae
Bof Franco <francobof01@gmail.com>
2024-09-28 16:32:32 -0300

    add 403

--- admin/src/web/handlers/error.py
+++ admin/src/web/handlers/error.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from flask import render_template
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 @dataclass
 class Error:
@@ -11,3 +13,10 @@ def error_not_found(e):
     error = Error(404, "Not Found", "The Requested URL was not found on the server.")
     
     return render_template('error.html', error=error), 404
+
+@app.errorhandler(403)
+def forbidden(e):
+    error_title = "Forbidden"
+    error_msg = "You shouldn't be here!"
+    return render_template('error.html',
+                           error_title=error_title,error_msg=error_msg), 403
