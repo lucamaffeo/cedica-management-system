219ab21740416ca68517175ec41ee36b762ec0e0
Luca Maffeo <125698908+lucamaffeo@users.noreply.github.com>
2024-10-03 14:37:51 -0300

    agrego index.html

--- admin/src/web/controllers/__init__.py
+++ admin/src/web/controllers/__init__.py
@@ -1,7 +1,9 @@
 from .auth import bp as auth_bp
 from .users import bp as users_bp
+from .employees import bp as employees_bp
 
 def register_blueprints(app):
     """Register all blueprints with the Flask app."""
     app.register_blueprint(auth_bp)
     app.register_blueprint(users_bp)
+    app.register_blueprint(employees_bp)
