583bfffda46ab2cb0aaac0310cb076b07042442a
Bof Franco <francobof01@gmail.com>
2024-10-13 17:59:51 -0300

    add recibos en menu

--- admin/src/web/controllers/__init__.py
+++ admin/src/web/controllers/__init__.py
@@ -2,6 +2,7 @@ from .auth import bp as auth
 from .users import bp as users
 from .payments import bp as payments
 from .employees import bp as employees
+from .receipt import bp as receipt
 
 def register_blueprints(app):
     """Register all blueprints with the Flask app."""
@@ -9,3 +10,4 @@ def register_blueprints(app):
     app.register_blueprint(users)
     app.register_blueprint(payments)
     app.register_blueprint(employees)
+    app.register_blueprint(receipt)
